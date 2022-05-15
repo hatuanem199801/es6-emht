54ab3ceebb18c44bf92ba2c28701399718af2004
Em Ha Tuan <emhat@nashtechglobal.com>
2022-05-15 09:32:22 +0700

    Extended Literals

---
+++ extendedLiterals/index.js
@@ -0,0 +1,3 @@
+// Acctually, I do not have to work in this feature so much.
+// So I dont have any idea for practice in this case.
+// Please, figure out in link http://es6-features.org/#UnicodeStringRegExpLiteral.
