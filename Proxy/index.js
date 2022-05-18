ca24c8cda2b81bbdc81458091ca4afdad9c76f4a
Em Ha Tuan <emhat@nashtechglobal.com>
2022-05-18 12:09:06 +0700

    Proxy

---
+++ Proxy/index.js
@@ -0,0 +1,13 @@
+// Hooking into runtime-level object meta-operations.
+
+let target = {
+    foo: "Welcome, foo"
+}
+let proxy = new Proxy(target, {
+    get (receiver, name) {
+        return name in receiver ? receiver[name] : `Hello, ${name}`
+    }
+})
+
+console.log(proxy.foo);
+console.log(proxy.world);
